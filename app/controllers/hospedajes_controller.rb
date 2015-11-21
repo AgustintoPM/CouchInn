e25d3acfa12f9049aec739cc2cba55e2cb2be42d
Maximiliano Alexandroff <alexandroff.max@gmail.com>
2015-11-21 13:20:58 -0300

    agregado de borrar un hospedaje, falta terminar

--- app/controllers/hospedajes_controller.rb
+++ app/controllers/hospedajes_controller.rb
@@ -1,5 +1,19 @@
 class HospedajesController < ApplicationController
+	before_action :get_hospedaje, only:[:edit, :update, :destroy]
+	
+	def get_hospedaje
+		@hospedaje = Hospedaje.find(params[:id])	
+	end
+	#editar un hospedaje
+	def edit
+		
+	end
+	#mostrar un hospedaje
 	def show
 		@hospedaje = Hospedaje.find(params[:id])
+	end
+	#borrar un hospedaje
+	def destroy
+		@hospedaje.destroy()
 	end
 end

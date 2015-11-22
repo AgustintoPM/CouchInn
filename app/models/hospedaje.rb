a7a7e59f1d7c36e5c14db1237d083db5829d9385
Maximiliano Alexandroff <alexandroff.max@gmail.com>
2015-11-22 11:37:33 -0300

    agregado de las relaciones en los modelos

--- app/models/hospedaje.rb
+++ app/models/hospedaje.rb
@@ -1,9 +1,10 @@
 class Hospedaje < ActiveRecord::Base
+  belongs_to :tipo
+  belongs_to :user
   validates :titulo, presence:true
   validates :foto, presence:true
   validates :lugar, presence:true
   validates :fecha, presence:true
   validates :capacidad, presence:true
   belongs_to :tipos
-    
 end

d864893c852c94edc3cc773951f10ee3ad74d823
AgustintoPM <agustin_rodrigombia@hotmail.com>
2015-11-23 13:00:26 -0300

    actualizando user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, format:{ with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
 	
+	#has_many :couches, dependent: :destroy
+
 	# Returns the hash digest of the given string.
 	def User.digest(string)
 	  cost = ActiveModel::SecurePassword.min_cost ?

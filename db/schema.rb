11f5c01ea2e1f40107461aeaf41cc35b6a746cb4
AgustintoPM <agustin_rodrigombia@hotmail.com>
2015-12-09 20:47:02 -0300

    arreglando el merge...feedbacks

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20151209021812) do
-=======
-ActiveRecord::Schema.define(version: 20151123231032) do
->>>>>>> calificaciones
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "comentarios", force: :cascade do |t|
     t.text     "opinion"
     t.string   "hospedaje_id"
@@ -72,21 +67,20 @@ ActiveRecord::Schema.define(version: 20151123231032) do
     t.string   "nombre"
     t.boolean  "disponible"
     t.datetime "fecha"
-=======
-  create_table "user_feedbacks", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
->>>>>>> calificaciones
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-=======
+  create_table "user_feedbacks", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_index "user_feedbacks", ["user_id", "created_at"], name: "index_user_feedbacks_on_user_id_and_created_at", using: :btree
   add_index "user_feedbacks", ["user_id"], name: "index_user_feedbacks_on_user_id", using: :btree
 
->>>>>>> calificaciones
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"

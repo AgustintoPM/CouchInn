5c35cb9d874d5d24c8db9890c1fd001d530397e1
AgustintoPM <agustin_rodrigombia@hotmail.com>
2015-12-10 18:00:11 -0300

    Activacion de cuenta via mail

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 # Turn off auto TLS for e-mail
-ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+#ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "https://gentle-spire-4321.herokuapp.com/",
+  :authentication => :plain,
+  :user_name => "angelicabufartanelo@gmail.com",
+  :password => "ingenieria.2015"
+}
